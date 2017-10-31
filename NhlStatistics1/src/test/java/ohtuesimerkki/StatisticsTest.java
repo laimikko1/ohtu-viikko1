a28a8c011bf64afc5f6527aeeb012cc0aae61432
laimikko1 <mikko.s.laitinen@helsinki.fi>
2017-10-31 21:49:00 +0200

    Änäritestattu

---
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,64 @@
+
+package ohtuesimerkki;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class StatisticsTest {
+    
+        Reader readerStub = new Reader() {
+            
+            @Override
+            public List<Player> getPlayers() {
+                ArrayList<Player> players = new ArrayList();
+                
+                players.add(new Player("Semenko", "EDM", 4, 12));
+                players.add(new Player("Lemieux", "PIT", 45, 54));
+                players.add(new Player("Kurri", "EDM", 37, 53));
+                players.add(new Player("Yzerman", "DET", 42, 56));
+                players.add(new Player("Gretzky", "EDM", 35, 89));
+
+                return players;
+            }
+        };
+        
+    
+    Statistics stats;
+        @Before
+        public void setUp() {
+            
+        stats = new Statistics(readerStub);
+    }
+        
+        
+        @Test
+        public void playerTestReturnsCorrect() {
+            Player p = stats.search("Semenko");
+            Assert.assertEquals("Semenko", p.getName());
+        }
+        
+        @Test
+        public void wrongPlayerTestReturnsNull() {
+            Player p = stats.search("Laitinen");
+            Assert.assertNull(p);
+        }
+        
+        @Test
+        public void playersOfTeamCorrect() {
+            List<Player> players = stats.team("PIT");
+        
+            Assert.assertEquals("Lemieux", players.get(0).getName());
+        }
+        
+        @Test
+        public void CorrectAmountOfTopScorers() {
+            List<Player> scorers = stats.topScorers(1);
+            Assert.assertEquals("Gretzky", scorers.get(0).getName());
+        }
+    
+}    
+  
